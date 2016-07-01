a1b4982bf5835ae1c23690351db0dc0eadbde63d
Russ Davies <russdavies392@gmail.com>
2016-07-01 11:14:39 -0700

    Add functionality where adding a pizza topping costs an extra dollar

--- js/scripts.js
+++ js/scripts.js
@@ -5,16 +5,31 @@ function Pizza (toppings, size) {
 }
 var pizzaCost = 0;
 
+// var toppingsSelection = function(toppings){
+//   if(this.toppings !== "None")
+//   pizzaCost += 1;
+//   console.log(pizzaCost);
+// }
+
 Pizza.prototype.pizzaPrice = function(price) {
   if (this.pizzaSize === "Large") {
     pizzaCost += 20;
     console.log(pizzaCost)
+    if(this.toppings !== "None")
+    pizzaCost += 1;
+    console.log(pizzaCost);
   } else if (this.pizzaSize === "Medium") {
     pizzaCost += 15;
     console.log(pizzaCost);
+      if(this.toppings !== "None")
+      pizzaCost += 1;
+      console.log(pizzaCost);
   } else if (this.pizzaSize === "Small") {
     pizzaCost += 12;
     console.log(pizzaCost);
+      if(this.toppings !== "None")
+      pizzaCost += 1;
+      console.log(pizzaCost);
   } else {
     console.log(0);
   }
