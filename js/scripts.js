b6eb445a23f39d78e93fa9fb7fa1427af4a27f75
Russ Davies <russdavies392@gmail.com>
2016-07-01 09:51:40 -0700

    Add basic back-end logic and UI

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+// Business Logic
+function Pizza (toppings, size) {
+  this.toppings = toppings;
+  this.pizzaSize = size;
+}
+//Need to collect inputs into newPizza, then create a prototype that gathers price based on choices
+var newPizza = new Pizza //inputToppings, inputSize//)
+Pizza.prototype.price = [];
+// User Interface Logic
+$(document).ready(function(){
+
+});
