9183be7758b704a147b25a3ce951cbc986dc92c1
Russ Davies <russdavies392@gmail.com>
2016-07-01 17:36:55 -0700

    Add delivery and pick-up options

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,12 @@
 // Business Logic
-function Pizza (toppings1, toppings2, toppings3, size) {
+function Pizza (toppings1, toppings2, toppings3, size, delivery) {
   this.toppings1 = toppings1;
   this.toppings2 = toppings2;
   this.toppings3 = toppings3;
   this.pizzaSize = size;
+  this.delivery = delivery;
 }
+
 var pizzaCost = 0;
 
 Pizza.prototype.pizzaPrice = function(price) {
@@ -26,20 +28,25 @@ Pizza.prototype.pizzaPrice = function(price) {
       }if(this.toppings3 !== "None") {
       pizzaCost += 1;
       }
+    if(this.delivery === "delivery") {
+      pizzaCost += 2;
+    }
 }
 
 // User Interface Logic
 $(document).ready(function(){
   $("#pizza-form").submit(function(event){
+    $("#pizza-image").fadeOut();
     event.preventDefault();
     var smallMediumLarge = $("#pizza-size").val();
     var topping1 = $("#pizza-toppings1").val();
     var topping2 = $("#pizza-toppings2").val();
     var topping3 = $("#pizza-toppings3").val();
-    var newPizza = new Pizza (topping1, topping2, topping3, smallMediumLarge);
+    var delivery = $("input:radio[name=delivery]:checked").val();
+    var newPizza = new Pizza (topping1, topping2, topping3, smallMediumLarge, delivery);
     newPizza.pizzaPrice();
     $("#totalCost").text("Your Order: One " + smallMediumLarge.toLowerCase() + " pizza with selected toppings totals to $" +  pizzaCost + ".00");
     pizzaCost = 0;
-    $("#pizza-image").show();
+    $("#pizza-image").fadeIn();
   });
 });
